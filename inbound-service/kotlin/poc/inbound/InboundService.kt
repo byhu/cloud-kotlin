ddd0ab7f871fa17e4ddc041ad2b30222ec1f58f8
byhu <yuanzhao.hu@gmail.com>
2018-11-29 09:38:40 -0500

    4

--- inbound-service/kotlin/poc/inbound/InboundService.kt
+++ inbound-service/kotlin/poc/inbound/InboundService.kt
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.annotation.Bean
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,7 +16,7 @@ class InboundService(val repo: TradeRepository, val kafkaTemplate: KafkaTemplate
 
     @KafkaListener(id = "inbound", topics = ["inbound"], containerFactory = "listenerContainerFactory")
     @Transactional("chainedTransactionManager")
-    fun inboundListener(record: ConsumerRecord<String, String>) = {
+    fun inboundListener(record: ConsumerRecord<String, String>) {
         val jsonMapper = ObjectMapper().apply {
             registerKotlinModule()
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
@@ -30,11 +27,11 @@ class InboundService(val repo: TradeRepository, val kafkaTemplate: KafkaTemplate
     }
 
     @KafkaListener(id = "pending", topics = ["inbound"], containerFactory = "listenerContainerFactory")
-    fun pendingListener(record: ConsumerRecord<String, String>) = {
+    fun pendingListener(record: ConsumerRecord<String, String>) {
         val jsonMapper = ObjectMapper().apply {
             registerKotlinModule()
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            setDateFormat(StdDateFormat())
+            dateFormat = StdDateFormat()
         }
         val saved = repo.save(jsonMapper.readValue(record.value(), Trade::class.java))
         kafkaTemplate.send("pending", record.key(), jsonMapper.writeValueAsString(saved))
