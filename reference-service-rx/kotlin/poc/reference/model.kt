370fec5262802f17399a22af002de1b150a5d73e
byhu <yuanzhao.hu@gmail.com>
2018-12-05 11:51:21 -0500

    4

--- reference-service-rx/kotlin/poc/reference/model.kt
+++ reference-service-rx/kotlin/poc/reference/model.kt
@@ -1,8 +1,7 @@
 package poc.reference
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 
-data class Security(@Id val cusip: String, val desc: String)
+data class Security(val cusip: String, val desc: String)
 
 interface SecurityRepository : ReactiveCrudRepository<Security, String>
