c5ca71f8a1d75d7fddbb5c5b301042270a76a983
byhu <yuanzhao.hu@gmail.com>
2018-12-05 12:24:22 -0500

    4

---
+++ settle-service/kotlin/poc/settle/SettleApplication.kt
@@ -0,0 +1,20 @@
+package poc.settle
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+
+
+@SpringBootApplication
+class SettleApplication {
+    @Bean
+    fun init(template: ReactiveMongoTemplate) = CommandLineRunner {
+//        template.find(Query.query(Criteria.where("receivedAt").lt("White")), Transaction.class.java);
+    }
+}
+
+fun main(args: Array<String>) = runApplication<SettleApplication>(*args)
